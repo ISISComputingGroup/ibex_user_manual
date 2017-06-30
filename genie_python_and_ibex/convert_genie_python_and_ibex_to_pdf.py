ad34bac06433629dbc8d2380a0fd9adf58a7c960
AdrianPotter <adrian.potter@tessella.com>
2017-06-30 14:51:43 +0100

    Move genie_python course to separate folder

--- genie_python_and_ibex/convert_genie_python_and_ibex_to_pdf.py
+++ genie_python_and_ibex/convert_genie_python_and_ibex_to_pdf.py
@@ -1,28 +1,75 @@
+import logging
 import os
+from rst2pdf.createpdf import RstToPdf
 
-from src.file_access import FileAccess
-from src.local_logger import LocalLogger
-from src.upgrade import Upgrade
-from src.upgrade_step_from_3p2p1 import UpgradeStepFrom3p2p1
-from src.upgrade_step_noop import UpgradeStepNoOp
 
-# A list of upgrade step tuples tuple is name of version to apply the upgrade to and upgrade class.
-# The last step should have an upgrade class of None (this is how it knows it has reached the end)
-# Upgrade steps will be executed in order from the configuration set in the configuration file.
-# To add a step which does nothing use UpgradeStepNoOp this is often used to get from the latest dev
-# configuration to the latest production configuration
-UPGRADE_STEPS = [
-    ("3.2.1", UpgradeStepFrom3p2p1()),
-    ("3.2.1.1", None),
-]
+class PDFGenerator(object):
+
+    REST_EXTENSION = ".rest"
+    PYTHON_EXTENSION = ".py"
+    LOG_DIR = 'logs'
+    OUTPUT_DIR = 'output'
+
+    def __init__(self):
+        self._initialise_logging()
+
+    def _initialise_logging(self):
+        from datetime import datetime as dt
+        logger = logging.getLogger('pdf_generator')
+        logger.setLevel(logging.DEBUG)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        fh = logging.FileHandler(
+            os.path.join(PDFGenerator.LOG_DIR, 'pdf_generator_{0}.log'.format(dt.now().strftime("%y_%m_%d_%H_%M_%S"))))
+        fh.setLevel(logging.DEBUG)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+
+        self.logger = logger
+
+    def create(self):
+
+        for path in self.get_source_files():
+
+            self.logger.info("Reading {0}".format(path))
+            try:
+                raw_text = open(path, "r").readlines()
+            except Exception as e:
+                self.logger.error("Unknown error reading {0}: {1}".format(path, e.message))
+
+            self.logger.info("Formatting {0}".format(path))
+            try:
+                formatted_text = self.format_rst(raw_text)
+            except Exception as e:
+                self.logger.error("Unknown error formatting {0}: {1}".format(path, e.message))
+
+            output = os.path.join(PDFGenerator.OUTPUT_DIR,
+                                  path.replace(PDFGenerator.REST_EXTENSION, PDFGenerator.PYTHON_EXTENSION))
+            self.logger.info("Creating pdf of {0} at {1}".format(path, output))
+            try:
+                RstToPdf.createPdf(text=formatted_text,output=output)
+            except Exception as e:
+                self.logger.error("Unknown error creating pdf of {0}: {1}".format(path, e.message))
+
+    def get_source_files(self):
+        self.logger.info("Getting source files from: {0}".format(os.getcwd()))
+        files = [f for f in os.getcwd() if f.endswith(PDFGenerator.REST_EXTENSION)]
+        self.logger.info("Found {0} restructured text files for conversion".format(len(files)))
+        return files
+
+    def format_rst(self, text):
+        return text
+
 
 if __name__ == "__main__":
-
-    config_root = os.path.abspath(os.path.join(os.environ["ICPCONFIGROOT"], os.pardir))
-    log_dir = os.path.join(os.environ["ICPVARDIR"], "logs", "upgrade")
-
-    logger = LocalLogger(log_dir)
-    file_access = FileAccess(logger, config_root)
-
-    upgrade = Upgrade(file_access=file_access, logger=logger, upgrade_steps=UPGRADE_STEPS)
-    exit(upgrade.upgrade())
+    PDFGenerator().create()
